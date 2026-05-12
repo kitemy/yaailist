7646cb9b56c4e39cdefe92d38841612045138aaf
kitemy <misser.ilyes2020@gmail.com>
2026-05-12 21:36:26 +0200

    Update score.js

--- js/score.js
+++ js/score.js
@@ -1,26 +1,6 @@
 const scale = 0;
 
 export function score(rank, percent, minPercent) {
-    if (rank > 150) {
-        return 0;
-    }
-    if (rank > 75 && percent < 100) {
-        return 0;
-    }
-
-    let score = (-25*Math.pow(rank-1, 0.4) + 200) *
-        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
-
-    score = Math.max(0, score);
-
-    if (percent != 100) {
-        return round(score - score / 3);
-    }
-
-    return Math.max(round(score), 0);
-}
-
-function score(rank, percent, minPercent) {
     if (rank > 150) return 0;
     if (rank > 75 && percent < 100) return 0;
 

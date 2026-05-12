766c520339b522a024695631f8375533ff96e4d5
kitemy <misser.ilyes2020@gmail.com>
2026-05-12 21:45:03 +0200

    Refactor score function for improved readability

--- js/score.js
+++ js/score.js
@@ -1,20 +1,13 @@
-export function score(rank, percent, minPercent) {
-    if (rank > 150) {
-        return 0;
-    }
+function score(rank, percent, minPercent) {
+    if (rank > 150) return 0;
+    if (rank > 75 && percent < 100) return 0;
 
-    if (rank > 75 && percent < 100) {
-        return 0;
-    }
-
-    let score = (-25 * Math.pow(rank - 1, 0.4) + 200) *
+    let s = (-25 * Math.pow(rank - 1, 0.4) + 200) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
-    score = Math.max(0, score);
+    s = Math.max(0, s);
 
-    if (percent !== 100) {
-        score = score - score / 3;
-    }
+    if (percent !== 100) s -= s / 3;
 
-    return Math.max(Math.round(score), 0);
+    return Math.max(Math.round(s), 0);
 }

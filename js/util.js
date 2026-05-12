ccd6516e2d2e152b8035911f24415cb8c75900b9
kitemy <misser.ilyes2020@gmail.com>
2026-05-12 20:47:09 +0200

    Refactor util.js by removing comments and updating shuffle
    Removed comments referencing Stack Overflow links and updated shuffle function.

--- js/util.js
+++ js/util.js
@@ -1,4 +1,3 @@
-// https://stackoverflow.com/questions/3452546/how-do-i-get-the-youtube-video-id-from-a-url
 export function getYoutubeIdFromUrl(url) {
     return url.match(
         /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/,
@@ -17,17 +16,13 @@ export function getThumbnailFromId(id) {
     return `https://img.youtube.com/vi/${id}/mqdefault.jpg`;
 }
 
-// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 export function shuffle(array) {
     let currentIndex = array.length, randomIndex;
 
-    // While there remain elements to shuffle.
     while (currentIndex != 0) {
-        // Pick a remaining element.
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex--;
-
-        // And swap it with the current element.
+        
         [array[currentIndex], array[randomIndex]] = [
             array[randomIndex],
             array[currentIndex],

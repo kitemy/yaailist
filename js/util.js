d5cc83e8e87d388572076052265d05c7e7b079dd
kitemy <misser.ilyes2020@gmail.com>
2026-05-12 22:19:39 +0200

    Refactor localize function for simpler formatting
    Update localize function to use default locale settings.

--- js/util.js
+++ js/util.js
@@ -9,7 +9,7 @@ export function embed(video) {
 }
 
 export function localize(num) {
-    return num.toLocaleString(undefined, { minimumFractionDigits: 3 });
+    return num.toLocaleString();
 }
 
 export function getThumbnailFromId(id) {

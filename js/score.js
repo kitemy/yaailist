35d05e91cb6a933a4bcfc4e613a8f28f2bd48c34
kitemy <misser.ilyes2020@gmail.com>
2026-05-12 20:48:29 +0200

    Change scale to 0 and update score calculation
    Updated the scale constant to 0 and modified the score calculation formula.

--- js/score.js
+++ js/score.js
@@ -1,15 +1,5 @@
-/**
- * Numbers of decimal digits to round to
- */
-const scale = 3;
+const scale = 0;
 
-/**
- * Calculate the score awarded when having a certain percentage on a list level
- * @param {Number} rank Position on the list
- * @param {Number} percent Percentage of completion
- * @param {Number} minPercent Minimum percentage required
- * @returns {Number}
- */
 export function score(rank, percent, minPercent) {
     if (rank > 150) {
         return 0;
@@ -18,13 +8,7 @@ export function score(rank, percent, minPercent) {
         return 0;
     }
 
-    // Old formula
-    /*
-    let score = (100 / Math.sqrt((rank - 1) / 50 + 0.444444) - 50) *
-        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
-    */
-    // New formula
-    let score = (-24.9975*Math.pow(rank-1, 0.4) + 200) *
+    let score = (-25*Math.pow(rank-1, 0.4) + 200) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);

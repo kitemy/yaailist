e22010b823c220d8455c736609ea879f0390608d
kitemy <misser.ilyes2020@gmail.com>
2026-05-12 22:11:13 +0200

    Refactor score function and remove round function
    Refactor score calculation and remove unused round function.

--- js/score.js
+++ js/score.js
@@ -1,5 +1,3 @@
-const scale = 0;
-
 export function score(rank, percent, minPercent) {
     if (rank > 150) {
         return 0;
@@ -7,32 +5,11 @@ export function score(rank, percent, minPercent) {
     if (rank > 75 && percent < 100) {
         return 0;
     }
-
-    let score = (-25*Math.pow(rank-1, 0.4) + 200) *
+    let score = (-25 * Math.pow(rank - 1, 0.4) + 200) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
-
     score = Math.max(0, score);
-
     if (percent != 100) {
-        return round(score - score / 3);
+        return Math.round(score - score / 3);
     }
-
-    return Math.max(round(score), 0);
-}
-
-export function round(num) {
-    if (!('' + num).includes('e')) {
-        return +(Math.round(num + 'e+' + scale) + 'e-' + scale);
-    } else {
-        var arr = ('' + num).split('e');
-        var sig = '';
-        if (+arr[1] + scale > 0) {
-            sig = '+';
-        }
-        return +(
-            Math.round(+arr[0] + 'e' + sig + (+arr[1] + scale)) +
-            'e-' +
-            scale
-        );
-    }
+    return Math.max(Math.round(score), 0);
 }

c5ea4e4a918e2c97ae7cbc86bcce8f571a7c4d74
kitemy <misser.ilyes2020@gmail.com>
2026-05-14 16:48:36 +0200

    Refactor leaderboard display for better formatting
    Updated leaderboard display to improve formatting and localization of scores.

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -51,11 +51,9 @@ export default {
                 <div class="player-container">
                     <div class="player">
 
-                        <h1>
-                            #{{ selected + 1 }} {{ entry.user }}
-                        </h1>
+                        <h1>#{{ selected + 1 }} {{ entry.user }}</h1>
 
-                        <h3>{{ entry.total }}</h3>
+                        <h3>{{ localize(entry.total) }} points</h3>
 
                         <h2 v-if="entry.verified.length > 0">
                             Verified ({{ entry.verified.length }})
@@ -63,16 +61,17 @@ export default {
 
                         <table class="table">
                             <tr v-for="score in entry.verified">
-                                <td class="rank">
-                                    <p>#{{ score.rank }}</p>
-                                </td>
+                                <td class="rank"><p>#{{ score.rank }}</p></td>
                                 <td class="level">
-                                    <a class="type-label-lg" target="_blank" :href="score.link">
-                                        {{ score.level }}
-                                    </a>
+                                    <p class="type-label-lg">{{ score.level }}</p>
                                 </td>
-                                <td class="score">
-                                    <p>+{{ localize(score.score) }}</p>
+                                <td class="score"><p>+{{ localize(score.score) }}</p></td>
+                                <td class="yt-btn">
+                                    <a v-if="score.link" target="_blank" :href="score.link">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                                            <path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"/>
+                                        </svg>
+                                    </a>
                                 </td>
                             </tr>
                         </table>
@@ -83,16 +82,17 @@ export default {
 
                         <table class="table">
                             <tr v-for="score in entry.completed">
-                                <td class="rank">
-                                    <p>#{{ score.rank }}</p>
-                                </td>
+                                <td class="rank"><p>#{{ score.rank }}</p></td>
                                 <td class="level">
-                                    <a class="type-label-lg" target="_blank" :href="score.link">
-                                        {{ score.level }}
-                                    </a>
+                                    <p class="type-label-lg">{{ score.level }}</p>
                                 </td>
-                                <td class="score">
-                                    <p>+{{ localize(score.score) }}</p>
+                                <td class="score"><p>+{{ localize(score.score) }}</p></td>
+                                <td class="yt-btn">
+                                    <a v-if="score.link" target="_blank" :href="score.link">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                                            <path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"/>
+                                        </svg>
+                                    </a>
                                 </td>
                             </tr>
                         </table>
@@ -103,16 +103,17 @@ export default {
 
                         <table class="table">
                             <tr v-for="score in entry.progressed">
-                                <td class="rank">
-                                    <p>#{{ score.rank }}</p>
-                                </td>
+                                <td class="rank"><p>#{{ score.rank }}</p></td>
                                 <td class="level">
-                                    <a class="type-label-lg" target="_blank" :href="score.link">
-                                        {{ score.percent }}% {{ score.level }}
-                                    </a>
+                                    <p class="type-label-lg">{{ score.percent }}% {{ score.level }}</p>
                                 </td>
-                                <td class="score">
-                                    <p>+{{ localize(score.score) }}</p>
+                                <td class="score"><p>+{{ localize(score.score) }}</p></td>
+                                <td class="yt-btn">
+                                    <a v-if="score.link" target="_blank" :href="score.link">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                                            <path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"/>
+                                        </svg>
+                                    </a>
                                 </td>
                             </tr>
                         </table>
